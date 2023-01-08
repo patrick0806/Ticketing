2798b4277f6723cf73a661cad33a6383f9208aad
Patrick <patrickk0806@hotmail.com>
2023-01-08 11:41:49 -0300

    feat: config ngnx ingress

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,7 +1,12 @@
 import express from "express";
 
 const app = express();
-
 app.use(express.json());
 
-app.listen(3000, () => console.log("Listening on port 3000"));
+app.get("/api/users/current-user", (req, res) => {
+  res.send("Hi there!");
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000!!!!!!!!");
+});
